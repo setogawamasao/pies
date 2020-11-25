eef988a1ebb6bb783930cfe6156e99e422b94f8c
masaosetogawa <ms.job.hunting@gmail.com>
2020-11-25 22:45:34 +0900

    add select

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@ class MyApp extends BaseApp {
   // X:赤 Y:緑 Z:青
   public axesHelper = new THREE.AxesHelper(10000);
   public controls = new OrbitControls(this.Camera, this.Renderer.domElement);
-  public geometryBox = new THREE.BoxGeometry(10, 50, 50);
+  public geometryBox = new THREE.BoxGeometry(50, 50, 50);
   public materialBox = new THREE.MeshPhongMaterial({ color: 0xff0000 });
   public box = new THREE.Mesh(this.geometryBox, this.materialBox);
   public geometry = new THREE.SphereBufferGeometry(500, 60, 40);
-  public texture = new THREE.TextureLoader().load(`./build/textures/${3}.jpg`);
+  public texture = new THREE.TextureLoader().load(`./build/textures/${4}.jpg`);
   public material = new THREE.MeshBasicMaterial({ map: this.texture });
   public mesh = new THREE.Mesh(this.geometry, this.material);
   public speed = 0;
@@ -18,6 +18,16 @@ class MyApp extends BaseApp {
   public touchStartPoint = new THREE.Vector2();
   public isMove = false;
   public velocity = 0.001;
+  //========================
+  public plane = new THREE.Plane();
+  public rayCaster = new THREE.Raycaster();
+  public touchPoint = new THREE.Vector2();
+  public touchPointRot = new THREE.Vector2();
+  public offset = new THREE.Vector3();
+  public intersection = new THREE.Vector3();
+  // オブジェクトを格納する配列
+  public objects = new Array<THREE.Object3D>();
+  public isTouchedTarget = false;
 
   public Setup(): void {
     console.log("This is Setup.");
@@ -39,9 +49,7 @@ class MyApp extends BaseApp {
     this.Light.color.set(0xffffff);
     this.geometry = new THREE.SphereBufferGeometry(500, 60, 40);
     this.geometry.scale(-1, 1, 1);
-
     this.box.position.set(0, 0, 0);
-
     this.texture = new THREE.TextureLoader().load(
       `./build/textures/${this.getRandomNumber()}.jpg`
     );
@@ -71,6 +79,7 @@ class MyApp extends BaseApp {
       const text = new THREE.Mesh(geometry, matLite);
       text.rotateY(Math.PI / 2);
       this.Scene.add(text);
+      this.objects.push(text);
     });
 
     const loader2 = new THREE.FontLoader();
@@ -98,18 +107,38 @@ class MyApp extends BaseApp {
       const text = new THREE.Mesh(geometry, matLite);
       text.rotateY(Math.PI / 2);
       this.Scene.add(text);
+      this.objects.push(text);
     });
 
     this.Scene.add(this.Light);
-    //this.Scene.add(this.box);
-    this.Scene.add(this.mesh);
+    this.Scene.add(this.box);
+    //this.Scene.add(this.mesh);
     //this.Scene.add(this.axesHelper);
     this.speed += 0.001;
+
+    this.objects.push();
   }
 
   public touchStart(event: TouchEvent): void {
-    console.log("start");
-    this.velocity = 1;
+    console.log("touch start");
+    // 平行移動させる平面の法線ベクトルをカメラの方向ベクトルに合わせる
+    this.Camera.getWorldDirection(this.plane.normal);
+    // 画面上で指を動かした量から、三次元空間の移動量を決定する
+    this.touchPoint.x = (event.touches[0].pageX / window.innerWidth) * 2 - 1;
+    this.touchPoint.y = -(event.touches[0].pageY / window.innerHeight) * 2 + 1;
+    // タッチ点とカメラのレイキャストを設定
+    this.rayCaster.setFromCamera(this.touchPoint, this.Camera);
+
+    // オブジェクト群から、マウスとカメラの方向ベクトルの先にあるオブジェクト(平行移動させる物体)を抽出する。
+    const intersects = this.rayCaster.intersectObjects(this.objects);
+
+    // マウスとカメラの方向ベクトルの先にオブジェクトがあったとき
+    if (intersects.length > 0) {
+      this.velocity = 1;
+      this.isTouchedTarget = true;
+      console.log("touched");
+    }
+
     this.controls.enabled = false;
   }
 
@@ -120,7 +149,7 @@ class MyApp extends BaseApp {
   }
 
   public touchEnd(event: TouchEvent): void {
-    console.log("end");
+    console.log("touch end");
     this.velocity = 0.001;
     this.controls.enabled = true;
     if (!this.isMove) {
@@ -137,8 +166,8 @@ class MyApp extends BaseApp {
   };
 
   public Update(): void {
-    // console.log(this.change);
-    if (this.change) {
+    if (this.change && this.isTouchedTarget) {
+      console.log("image change");
       this.Scene.remove(this.mesh);
       this.texture = new THREE.TextureLoader().load(
         `./build/textures/${this.getRandomNumber()}.jpg`
@@ -146,7 +175,9 @@ class MyApp extends BaseApp {
       this.material = new THREE.MeshBasicMaterial({ map: this.texture });
       this.mesh = new THREE.Mesh(this.geometry, this.material);
       this.Scene.add(this.mesh);
+
       this.change = false;
+      this.isTouchedTarget = false;
     }
 
     this.speed += this.velocity;
